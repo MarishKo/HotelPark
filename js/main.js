4bf399137e76e3a37ccc1368e10c839a977b8d0e
MarishKo <deifos@mail.ru>
2015-11-15 14:45:35 +0300

    add all files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function() {  
+	
+	
+	var slick = $('.hotel-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		dots: true,
+	});
+	
+
+});
+
+
+
